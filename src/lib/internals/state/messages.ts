8895641d371e6b45d41df017f0e1897161981e79
iam-kevin <kevin.al.james@gmail.com>
2021-02-18 18:54:41 +0300

    added basic messages data

--- src/lib/internals/state/messages.ts
+++ src/lib/internals/state/messages.ts
@@ -1,3 +1,16 @@
 import { atom } from 'jotai'
+import { Potato } from '../../../../@types/index'
 
+const messageContextAtom = atom<Potato.MessageContext | undefined>(undefined)
+
+/**
+ * messages of the application
+ */
+export const messageAtom = atom(get => get(messageContextAtom)?.messages)
+
+/**
+ * Get the message by the message id
+ * @param messageId message
+ */
+export const getMessage = (messageId: number) => atom(get => get(messageAtom)?.[messageId])
 
