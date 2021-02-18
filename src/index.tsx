c6c0b823bf6d00d4161ea9c89656eda9795cff24
iam-kevin <kevin.al.james@gmail.com>
2021-02-18 18:06:52 +0300

    removed ChatFrame

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { Provider } from 'jotai'
 
 
-export default function Potato () {
+export default function Potato ({ children }: any) {
     return(
         <Provider>
-            
+            {children}
         </Provider>
     )
 }
